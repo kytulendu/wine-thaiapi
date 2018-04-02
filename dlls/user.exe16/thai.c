3aad392148860eafb837a861627d88e14c98a888
Khralkatorrix <garuda2550@gmail.com>
2018-04-02 17:56:50 +0700

    user.exe16: Fix warning.
    Signed-off-by: Khralkatorrix <garuda2550@gmail.com>

--- dlls/user.exe16/thai.c
+++ dlls/user.exe16/thai.c
@@ -19,13 +19,13 @@
  */
 
 #include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "windef.h"
 #include "winbase.h"
 #include "winuser.h"
 #include "wine/winuser16.h"
-
 #include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(user);
